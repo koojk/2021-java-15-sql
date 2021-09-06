1409c6729c4791908d04a971094da45d9abb6dff
koojk <kjh132465@gmail.com>
2021-09-06 18:04:00 +0900

    페이저까지 구현

--- routes/book/create-router.js
+++ routes/book/create-router.js
@@ -1,16 +1,16 @@
-const path = require('path')
 const express = require('express')
 const router = express.Router()
-const { error } = require('../../modules/util-module')
-const { pool } = require('../../modules/mysql-module')
+const { error } = require('../../modules/util')
+const { pool } = require('../../modules/mysql-init')
 
 router.post('/', async (req, res, next) => {
 	try {
 		const { title, writer, content } = req.body
 		const sql = 'INSERT INTO books SET title=?, writer=?, content=?'
 		const values = [title, writer, content]
-		const rs = await pool.execute(sql, values)
-		res.redirect('/book/list')
+		const [rs] = await pool.execute(sql, values)
+		if(rs.affectedRows === 1) res.redirect('/book')
+		else next(error(500, '데이터가 저장되지 않았습니다.'))
 	}
 	catch(err) {
 		next(error(500, err))
