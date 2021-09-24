f2e5f970cefbe209d32eb903017dac9ad8337618
koojk <kjh132465@gmail.com>
2021-09-24 17:12:41 +0900

    MVC 만드는 중

--- models/auth/index.js
+++ models/auth/index.js
@@ -1,4 +1,5 @@
 // findUser, verifyData, createUser, updateUser, deleteUser
 const createUser = require('./create-user')
+const findUser = require('./find-user')
 
-module.exports = { createUser }
+module.exports = { createUser, findUser }
--- routes/api/auth/verify-router.js
+++ routes/api/auth/verify-router.js
@@ -3,24 +3,18 @@ const express = require('express')
 const router = express.Router()
 const { error } = require('../../../modules/util')
 const { mysql, pool } = require('../../../modules/mysql-init')
+const { findUser } = require('../../../models/auth')
 
 router.get('/verify', async (req, res, next) => {
 	// userid, email 중복 검증
-	let sql
-	let key = req.query.key
-	let value = req.query.value
 	try {
-		if(value.includes("'") || value.includes('"')) {
-			res.status(400).json({ msg: '특수문자가 포함되어 있습니다.' })
-		}
-		else {
-			sql = ` SELECT * FROM users WHERE ${key} = ? `
-			const [rs] = await pool.execute(sql, [value])
-			res.status(200).json({ isUsed: rs.length ? true : false })
-		}
+		const r = await findUser(req.query.key, req.query.value)
+		if(r.success) res.redirect('/')
+		else if(r.msg) res.send(alert(r.msg))
+		else next(createError(r.err))
 	}
 	catch(err) {
-		res.status(500).json({ msg: '서버오류 입니다. 다시 시도해 주세요.', err })
+		next(createError(err))
 	}
 })
 
