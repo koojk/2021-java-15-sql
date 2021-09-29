e61dcff080a7435a57ae2bb40464467132dfcac6
koojk <kljh0881@gmail.com>
2021-09-29 23:59:17 +0900

    수정

--- passport/naver-strategy.js
+++ passport/naver-strategy.js
@@ -1,13 +1,39 @@
-const path = require('path')
-const express = require('express')
-const router = express.Router()
-const passport = require('passport')
-const { error, alert } = require('../../modules/util')
+const NaverStrategy = require('passport-naver').Strategy
+const { createSnsUser, existUser } = require('../models/auth')
 
-router.get('/', passport.authenticate('naver'))
+const cb = async (accessToken, refreshToken, profile, done) => {
+	try {
+		// console.log('==================')
+		// console.log(accessToken)
+		// console.log(refreshToken)
+		// console.log(profile)
+		// console.log('==================')
+		let user = { userid: profile.id, accessToken }
+		let userSns = { accessToken, refreshToken, provider: 'NA', snsid: profile.id, 
+			snsName: profile._json.nickname || null,
+			displayName: profile.displayName || null,
+			profileURL: profile._json.profile_image || null,
+			email: profile._json.email || null,
+		}
+		let { success, idx } = await existUser('userid', user.userid)
+		if(success) {
+			user.idx = idx
+		}
+		else {
+			let { idx: id } = await createSnsUser(user, userSns)
+			user.idx = id
+		}
+		done(null, user)
+	}
+	catch(err) {
+		done(err)
+	}
+}
 
-router.get('/cb', passport.authenticate('naver', {failureRedirect: '/'}), (req, res, next) => {
-	res.send(alert('로그인 되었습니다.'))
-})
+const naverStrategy = new NaverStrategy({
+	clientID: process.env.NAVER_KEY,
+	clientSecret: process.env.NAVER_SALT,
+	callbackURL: '/auth/naver/cb'
+}, cb)
 
-module.exports = router
+module.exports = passport => passport.use(naverStrategy)
