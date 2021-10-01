bcca72a717221d8741608b2ef2a71d78ff5a2d8e
koojk <kljh0881@gmail.com>
2021-10-01 11:23:20 +0900

    mypage ejs init

--- back/modules/lang-init.js
+++ back/modules/lang-init.js
@@ -68,6 +68,15 @@ module.exports = _lang => {
 					USERNAME			: '이름',
 					EMAIL					: '이메일',
 				},
+				MYPAGE : {
+					TITLE 				: '회원 정보 수정',
+					DESC 					: '수정할 내용을 아래에서 변경하세요.',
+					USERID				: '아이디',
+					PASSWD				: '패스워드',
+					PASSWD2				: '패스워드 확인',
+					USERNAME			: '이름',
+					EMAIL					: '이메일',
+				},
 				BT: {
 					UPDATE				: '수정',
 					DELETE				: '삭제',
@@ -76,6 +85,7 @@ module.exports = _lang => {
 					RESET					: '다시 등록',
 					JOIN					: '회원 가입',
 					LOGIN					: '회원 로그인',
+					BACK					: '뒤로 가기',
 				},
 				MSG: {
 					DELETE				: '정말로 삭제하시겠습니까?'
@@ -148,6 +158,15 @@ module.exports = _lang => {
 					USERNAME			: 'User name',
 					EMAIL					: 'Email',
 				},
+				MYPAGE : {
+					TITLE 				: 'User Information Update',
+					DESC 					: 'Change the contents of the user information to be edited below.',
+					USERID				: 'User id',
+					PASSWD				: 'Password',
+					PASSWD2				: 'Password confirm',
+					USERNAME			: 'User name',
+					EMAIL					: 'Email',
+				},
 				BT: {
 					UPDATE				: 'UPDATE',
 					DELETE				: 'DELETE',
@@ -156,6 +175,7 @@ module.exports = _lang => {
 					RESET					: 'RESET',
 					JOIN					: 'JOIN',
 					LOGIN					: 'LOGIN',
+					BACK					: 'BACK',
 				},
 				MSG: {
 					DELETE				: 'Are you sure you want to delete it?'
