f0555ea5e4fd19670b32ba2f8df8d9ae6c1f907f
koojk <kljh0881@gmail.com>
2021-10-19 10:06:29 +0900

    회원탈퇴 디자인 완료

--- back/modules/lang-init.js
+++ back/modules/lang-init.js
@@ -53,6 +53,10 @@ module.exports = _lang => {
 					TITLE 				: '회원 로그인',
 					DESC 					: '회원님의 로그인 정보를 아래에 입력해 주세요.',
 				},
+				WITHDRAWAL: {
+					TITLE 				: '회원 탈퇴',
+					DESC 					: '회원 탈퇴를 원하시면 아래의 정보를 입력해 주세요.',
+				},
 				FIELD : {
 					NO						: '번호',
 					TITLE					: '제목',
@@ -69,6 +73,7 @@ module.exports = _lang => {
 					PASSWD2				: '패스워드 확인',
 					USERNAME			: '이름',
 					EMAIL					: '이메일',
+					MSG						: '회원 탈퇴 사유',
 				},
 				MYPAGE : {
 					TITLE 				: '회원 정보 수정',
@@ -90,8 +95,8 @@ module.exports = _lang => {
 					LIST					: '리스트',
 					RESET					: '다시 등록',
 					JOIN					: '회원 가입',
-					WITHDRAWAL					: '회원 탈퇴',
 					LOGIN					: '회원 로그인',
+					WITHDRAWAL		: '회원 탈퇴',
 					BACK					: '뒤로 가기',
 					APIKEY				: '인증키 생성/변경',
 				},
@@ -125,7 +130,7 @@ module.exports = _lang => {
 				GLOBAL: {
 					LOGO					: 'BOOK MANAGEMENT SYSTEM',
 					TAB_TITLE			: 'Express Board',
-					NAVI: 				['Book Register', 'Book List', 'Login', 'Logout', 'Join', 'Mypage', 'Edit User', 'Book List(VUE)']
+					NAVI: 				['Book Register', 'Book List', 'Login', 'Logout', 'Join', 'Mypage', 'Edit User', 'Book List(VUE)', 'Withdrawal']
 				},
 				LIST: {
 					TITLE 				: 'Book List',
@@ -151,6 +156,10 @@ module.exports = _lang => {
 					TITLE 				: 'Member Login',
 					DESC 					: 'Please enter your login information below.',
 				},
+				WITHDRAWAL: {
+					TITLE 				: 'Withdrawal',
+					DESC 					: 'If you wish to cancel your membership, please enter the information below.',
+				},
 				FIELD : {
 					NO						: 'No',
 					TITLE					: 'Title',
@@ -167,6 +176,7 @@ module.exports = _lang => {
 					PASSWD2				: 'Password confirm',
 					USERNAME			: 'User name',
 					EMAIL					: 'Email',
+					MSG						: 'Reason for withdrawal'
 				},
 				MYPAGE : {
 					TITLE 				: 'User Information Update',
@@ -188,8 +198,8 @@ module.exports = _lang => {
 					LIST					: 'LIST',
 					RESET					: 'RESET',
 					JOIN					: 'JOIN',
-					WITHDRAWAL	: 'WITHDRAWAL',
 					LOGIN					: 'LOGIN',
+					WITHDRAWAL		: 'WITHDRAWAL',
 					BACK					: 'BACK',
 					APIKEY				: 'Auth-key create/update',
 				},
